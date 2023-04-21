61c93a7df0c59cdc0ce945090137a1ebeaa4ec96
hyuntae <sotae69@naver.com>
2023-04-21 11:15:51 +0900

    최초 커밋

---
+++ src/main/java/com/example/algorithmproblem/AlgorithmProblemApplication.java
@@ -0,0 +1,13 @@
+package com.example.algorithmproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmProblemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmProblemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/algorithmproblem/baekjoon/problem27960.java
@@ -0,0 +1,9 @@
+package com.example.algorithmproblem.baekjoon;
+
+public class problem27960 {
+
+    /* 27960 문제 */
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
