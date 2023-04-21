d4ad036fc5d5ea117e78a176e22c18f466edb2ee
hyuntae <33915266+sohyuntae@users.noreply.github.com>
2023-04-21 15:29:03 +0900

    Update 문자열 겹쳐쓰기.java

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -13,6 +13,11 @@ class Solution {
             }
         }
         
+        // 추가
+        // String before = my_string.substring(0, s);
+        // String after = my_string.substring(s + overwrite_string.length());
+        // return before + overwrite_string + after;
+        
         return answer;
     }
 }
