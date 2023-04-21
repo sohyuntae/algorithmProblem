0ae66025ce06341be1bde64861136b780a8aba95
hyuntae <sotae69@naver.com>
2023-04-21 13:19:39 +0900

    27960번 문제 해결.

--- src/main/java/com/example/algorithmproblem/baekjoon/problem27960.java
+++ src/main/java/com/example/algorithmproblem/baekjoon/problem27960.java
@@ -2,8 +2,16 @@ package com.example.algorithmproblem.baekjoon;
 
 public class problem27960 {
 
-    /* 27960 문제 */
+    /* 27960 문제
+    *  xor 연산 이용
+    * */
     public static void main(String[] args) {
-        System.out.println("Test");
+
+        String x = "55 73";
+
+        int a = Integer.parseInt(x.split(" ")[0]);
+        int b = Integer.parseInt(x.split(" ")[1]);
+
+        System.out.println(a ^ b);
     }
 }
