55acc6ca4a11252e1987fbfaa54b5688a06dd39c
hyuntae <33915266+sohyuntae@users.noreply.github.com>
2023-05-09 11:07:18 +0900

    Update 수열과 구간 쿼리 2.kt
    다른 개발자가 푼 내용 정리.

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.kt
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.kt
@@ -10,6 +10,13 @@ class Solution {
             answer = if (temp == null) answer.plus(-1) else answer.plus(temp)
         }
         
+        
+        // 다른분들이 푼것 내용 체크
+        queries.map { q ->
+            val min = arr.sliceArray(q[0]..q[1]).filter { it > q[2] }.minOrNull() ?: -1
+            return@map if (min == Int.MAX_VALUE) -1 else min
+        }
+        
         return answer
     }
 }
