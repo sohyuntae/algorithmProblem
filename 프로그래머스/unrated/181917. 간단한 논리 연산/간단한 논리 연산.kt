2e25700a0fb907b2d6347c94a22a51b103094c2b
hyuntae <33915266+sohyuntae@users.noreply.github.com>
2023-05-10 13:16:49 +0900

    [unrated] Title: 간단한 논리 연산, Time: 0.01 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.kt
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.kt
@@ -1,28 +1,5 @@
 class Solution {
     fun solution(x1: Boolean, x2: Boolean, x3: Boolean, x4: Boolean): Boolean {
-        var answer: Boolean = true
-        
-        var temp1: Boolean
-        var temp2: Boolean
-
-        if (x1 == x2) {
-            temp1 = x1
-        } else {
-            temp1 = true
-        }
-
-        if (x3 == x4) {
-            temp2 = x3
-        } else {
-            temp2 = true
-        }
-        
-        if (temp1 == temp2) {
-            answer = temp1
-        } else {
-            answer = false
-        }
-        
-        return answer
+        return (x1 || x2) && (x3 || x4)
     }
 }
